0a36213d0ae8f7e079680f5adf09d57996565f28
GabiOC <oconnor.gabriele@gmail.com>
2015-08-11 15:46:38 -0400

    add trending methods

--- lib/program.rb
+++ lib/program.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 
 def start_giphinator
-  puts "Welcome! Here are your options:
+  puts "Here are your options:
   - search
   - trending
   - random
   - translate
-  -exit"
+  - exit"
   answer = gets.chomp.downcase
   handle_response(answer)
 end
@@ -26,6 +26,8 @@ def handle_response(answer)
     exit
   else
     puts "Please enter valid command: search, trending, random, stickers, translate"
+    answer = gets.chomp.downcase
+    handle_response(answer)
   end
 end
 
@@ -35,13 +37,11 @@ def search
   answer = gets.chomp
   gif = Giphy::Client.new
   data = gif.search("#{answer}")
-  puts get_url(data).sample
+  url = get_url(data).sample
+  puts "Here is the link: 
+  #{url}"
+  open_choice(url)
 end
-
-# search {
-#   answer = gets.chomp
-#   gif = Giphy::Client.new
-# }
 
 def trending
   puts "TRENDING
@@ -49,17 +49,23 @@ def trending
   answer = gets.chomp.to_i
   gif = Giphy::Client.new
   data = gif.trending(limit: answer)
-  puts get_url(data)
+  url = get_url(data)
+  puts "Here are the links:"
+  url.each_with_index do |item, index|
+    puts "#{index+1}. #{item}"
+  end
+  open_trending(url)
 end
-
 
 def random
   puts "RANDOM"
   gif = Giphy::Client.new
   data = gif.random
-  puts data["image_original_url"]
+  url = data["image_original_url"]
+  puts "Here is the link: 
+  #{url}"
+  open_choice(url)
 end
-
 
 def translate
   puts "TRANSLATE
@@ -67,13 +73,11 @@ def translate
   answer = gets.chomp
   gif = Giphy::Client.new
   data = gif.translate("#{answer}")
-  puts data["images"]["fixed_height"]["url"]
+  url = data["images"]["fixed_height"]["url"]
+  puts "Here is the link: 
+  #{url}"
+  open_choice(url)
 end
-
-# def handle_gifing
-#   answer = gets.chomp
-#   gif = Giphy::Client.new
-# end
 
 def exit
   abort("Thanks for gifing!")
@@ -87,25 +91,40 @@ def get_url(data)
   arr
 end
 
+def open_url(url)
+  cmd = "open #{url}"
+  `#{cmd}`
+end
 
+def open_choice(url)
+  puts "Would you like to open? (yes/no)"
+  answer = gets.chomp.downcase
+  if answer == "yes"
+    open_url(url)
+    start_giphinator
+  elsif answer == "no"
+    start_giphinator
+  else
+    puts "Enter yes or no dummy!"
+    open_choice(url)
+  end
+end
 
-# gif = Giphy::Client.new
-
-# data = gif.search("computer", {rating: "r"})
-
-# binding.pry
-
-## FOR TRANSLATE
-# puts data["images"]["original"]["url"]
-
-# arr = []
-
-# data.each do |x|
-#   arr << x["images"]["original"]["url"]
-# end
-
-# puts arr
-
+def open_trending(url)
+   puts "Do you want to open one? (yes/no)"
+  answer = gets.chomp.downcase
+  if answer == "yes"
+    puts "Which one do you want to open? (type number)"
+    answer = gets.chomp.to_i
+    choice = url[answer-1]
+    open_url(choice)
+    start_giphinator
+  elsif answer == "no"
+    start_giphinator
+  else puts "Enter yes or no dummy!"
+    open_trending(url)
+  end
+end
 
 
 
